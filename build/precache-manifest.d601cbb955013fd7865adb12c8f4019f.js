01bc8e3cc220847bc19b0e4597b5406570d1cb3a
JaKel Jenkins <jakelj24@gmail.com>
2020-12-24 15:11:10 -0500

    updated

---
+++ build/precache-manifest.d601cbb955013fd7865adb12c8f4019f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07acc9d3ab98011346e8c96f70ec36b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6b2251b8558b8dabaa5",
+    "url": "/static/css/main.86a512e3.chunk.css"
+  },
+  {
+    "revision": "bc2c56c66791fbf3392e",
+    "url": "/static/js/2.b5e3f079.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.b5e3f079.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6b2251b8558b8dabaa5",
+    "url": "/static/js/main.b2402c12.chunk.js"
+  },
+  {
+    "revision": "72c5965cbfb3ac6a55d7",
+    "url": "/static/js/runtime-main.40d8ffab.js"
+  }
+]);
